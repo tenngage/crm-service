ef61f458c5a4eaca70b92259df59bf96baf1b842
tenngage <ibi0880@gmail.com>
2025-09-18 21:16:06 +0300

    Added tests for login function

--- tests/integration/test_api/test_users.py
+++ tests/integration/test_api/test_users.py
@@ -3,26 +3,45 @@ import pytest
 
 class TestUserEndpoints:
     @pytest.mark.asyncio
-    async def test_register_user_success(self, get_test_db):
-        from src.services.user_services import register
-        from src.schemas.user_schemas import UserRegister
+    async def test_register_user_success(
+        self,
+        get_test_db,
+        test_user_data,
+        register_test_user,
+    ):
+        # Registering user for tests
+        result = register_test_user
 
-        user_data = UserRegister(
-            email="email123@gmail.com",
-            username="user123",
-            password="password123",
-        )
-
-        result = await register(get_test_db, user_data)
-
-        assert result.email == user_data.email
-        assert result.username == user_data.username
+        assert result.email == test_user_data["email"]
+        assert result.username == test_user_data["username"]
 
         from sqlalchemy import select
         from src.models.user import User
 
+        # Check if user in db matches test user
         db_result = await get_test_db.execute(select(User))
         users = db_result.scalars().all()
         assert len(users) == 1
-        assert users[0].email == user_data.email
-        assert users[0].username == user_data.username
+        assert users[0].email == test_user_data["email"]
+        assert users[0].username == test_user_data["username"]
+
+    @pytest.mark.asyncio
+    async def test_login_user_success(
+        self,
+        get_test_db,
+        test_user_data,
+    ):
+        from src.services.user_services import login
+        from fastapi.security import OAuth2PasswordRequestForm
+
+        # Mocking form_data
+        form_data = OAuth2PasswordRequestForm(
+            username=test_user_data["username"],
+            password=test_user_data["password"],
+            scope=""
+        )
+
+        # Trying to login
+        result = await login(form_data, get_test_db)
+        assert result.access_token
+        assert result.token_type == "bearer"
